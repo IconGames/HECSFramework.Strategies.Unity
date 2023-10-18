f90c8b922111e0a604dbb50bddcdc2c5226468a4
Brightori <el.brightori@gmail.com>
2023-10-18 09:29:51 +0300

    upd: rotate to target now have additional bool - is complete rotating or not

--- Nodes/RotateToTargetNode.cs
+++ Nodes/RotateToTargetNode.cs
@@ -14,6 +14,8 @@ namespace Strategies
         [Connection(ConnectionPointType.In, "<Transform> Transform to rotation")]
         public GenericNode<Transform> TransformSource;
 
+        [Connection(ConnectionPointType.Out, "<bool> IsRotatingComplete(by ConvertNode)")]
+        public BaseDecisionNode IsRotatingOut;
 
         [ExposeField]
         public bool FixYAxis = true;
@@ -23,7 +25,7 @@ namespace Strategies
         protected override void Run(Entity entity)
         {
             var targetTransform = Target.Value(entity).GetComponent<UnityTransformComponent>().Transform;
-            var entityTransform = TransformSource.Value(entity);
+            var entityTransform = TransformSource != null ? TransformSource.Value(entity) : entity.GetComponent<UnityTransformComponent>().Transform;
             var posTarget = targetTransform.position;
             var pos = entityTransform.position;
 
@@ -36,6 +38,26 @@ namespace Strategies
             var quaternion = Quaternion.LookRotation(dir, Vector3.up);
             entityTransform.rotation = Quaternion.RotateTowards(entityTransform.rotation, quaternion, RotationSpeed.Value(entity)*Time.deltaTime);
             Next.Execute(entity);
+        }
+
+        [GetConvertNodeAttribue]
+        public bool IsRotatingComplete(Entity entity)
+        {
+            var targetTransform = Target.Value(entity).GetComponent<UnityTransformComponent>().Transform;
+            var entityTransform = TransformSource != null ? TransformSource.Value(entity) : entity.GetComponent<UnityTransformComponent>().Transform;
+
+            var posTarget = targetTransform.position;
+            var pos = entityTransform.position;
+
+            var dir = posTarget - pos;
+            var forward = entityTransform.forward;
+
+            if (Vector3.Angle(forward, dir) < 0.05f)
+            {
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
